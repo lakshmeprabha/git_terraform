a0a91033fe6a910c6518bdf3aaaeae16ffe1aaf1
lakshmeprabha <prabhamani1998@gmail.com>
2023-06-20 10:01:06 +0530

    Initial Commit

---
+++ provided.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = var.region
+  access_key = "AKIA24P4J4NDFXIJY3SV"
+  secret_key = "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"
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0607784b46cbe5816"
+}
