bb111a917dd8e4fa75a4370d62bcd9881d18ea2b
lakshmeprabha <prabhamani1998@gmail.com>
2023-07-03 14:00:51 +0530

    ec2 updated

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,22 @@
 resource "aws_instance" "firstec2" {
   ami           = var.ami_id
   instance_type = "t2.micro"
-  tags = {
-    Name = "First ec2"
+   key_name      = "terraform-key"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./terraform-key.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update",
+      "sudo yum install docker -y",
+      "sudo service docker start",
+      "sudo docker pull prabhamani/angularimage:v1",
+      "sudo docker run -d -it --name angular_container -p 80:80 prabhamani/angularimage:v1"
+    ]
   }
 }
-
